0b6b81812829b421ce9214c258b327f83ca3f8e3
sevenxylee <2218259199@qq.com>
2022-11-20 22:48:29 +0800

    添加商铺信息缓存和商铺类别缓存

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -14,7 +24,25 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result getShopById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        String shopCache = stringRedisTemplate.opsForValue().get(key);
+        if(shopCache!=null && !shopCache.isEmpty()){
+            log.info("Redis cache hit. key is : {}",key);
+            return Result.ok(JSON.parseObject(shopCache,Shop.class));
+        }
+        Shop shop = getById(id);
+        if(shop==null){
+            return Result.fail("店铺信息不存在！");
+        }
+        stringRedisTemplate.opsForValue().set(key,JSON.toJSONString(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,26 @@
 package com.hmdp.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPES_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPES_TTL;
 
 /**
  * <p>
@@ -14,7 +30,35 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+        String key = CACHE_SHOP_TYPES_KEY;
+        Set<String> typeList = stringRedisTemplate.opsForZSet().range(key, 0, 9);
+        if (typeList!=null && !typeList.isEmpty()){
+            log.info("Redis cache hit. key is : {}", key);
+            ArrayList<ShopType> list = new ArrayList<>();
+            typeList.forEach(e->{
+                list.add(JSON.parseObject(e,ShopType.class));
+            });
+            return Result.ok(list);
+        }
+        List<ShopType> queryList = query().orderByDesc("sort").list();
+        if(queryList==null){
+            return Result.fail("查询商户分类失败！");
+        }
+        queryList.forEach(e->{
+            stringRedisTemplate.opsForZSet().add(key, JSON.toJSONString(e),e.getSort());
+        });
+        stringRedisTemplate.opsForZSet().getOperations().expire(key,CACHE_SHOP_TYPES_TTL, TimeUnit.MINUTES);
+
+        return Result.ok(queryList);
+
+    }
 }
