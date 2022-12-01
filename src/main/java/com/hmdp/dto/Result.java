4a9d97aa92f92963f1402b05c9a25adabc68bf8e
lxeey <sevenxylee@163.com>
2022-12-01 16:13:46 +0800

    使用泛型做统一返回处理

--- src/main/java/com/hmdp/dto/Result.java
+++ src/main/java/com/hmdp/dto/Result.java
@@ -9,22 +9,22 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
+public class Result<T> {
     private Boolean success;
     private String errorMsg;
-    private Object data;
+    private T data;
     private Long total;
 
-    public static Result ok(){
+    public static <T> Result<T> ok(){
         return new Result(true, null, null, null);
     }
-    public static Result ok(Object data){
+    public static <T> Result<T> ok(T data){
         return new Result(true, null, data, null);
     }
-    public static Result ok(List<?> data, Long total){
+    public static <T> Result<T> ok(List<T> data, Long total){
         return new Result(true, null, data, total);
     }
-    public static Result fail(String errorMsg){
+    public static <T> Result<T> fail(String errorMsg){
         return new Result(false, errorMsg, null, null);
     }
 }
